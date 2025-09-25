fcb83e599cee3d8f7f201e25eeb47cfbe7b14541
Will <wylin@stu.pku.edu.cn>
2025-09-25 16:40:13 -0400

    Fix: Make venue optional for talkmap.ipynb

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,8 @@
 var addressPoints = [
   [
-    "2024 Lin-Bridge Exoplanet Symposium<br />SYSU, Zhuhai, China; \u5e7f\u4e1c\u7701\u73e0\u6d77\u5e02\u4e2d\u5c71\u5927\u5b66",
-    22.3461238,
-    113.5875859
+    "2024 Lin-Bridge Exoplanet Symposium<br />None; Zhongshan University, Zhuhai, Guangdong, China",
+    22.351008,
+    113.5864644
   ],
   [
     "2024 National Conferennce in Planetary Sciences<br />Nanjing; \u6c5f\u82cf\u7701\u5357\u4eac\u5e02",
